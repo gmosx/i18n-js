1f2343f638debb98bb7533c6a4d0df01e5d422e8
George Moschovitis <george.moschovitis@gmail.com>
2019-06-20 17:37:04 +0300

    add l10n tag, more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,40 @@
-import { l10nDictionary, localizeText } from "./index";
+import { l10n, localizeText, setLocalizationDictionary } from "./index";
 
-test("localizeText should localize simple strings", () => {
-    l10nDictionary["Hello"] = "Γεια σας"
+const greekDictionary = {
+    "Hello": "Γεια σας",
+    "{0} notifications are available": "{0} ειδοποιήσεις είναι διαθέσιμες",
+    "I can change order {0}": "{0} μπορώ να αλλάξω σειρά",
+    "I have {0} boxes and {1} balls": "Έχω {0} κουτιά και {1} μπάλλες",
+}
+
+test("localizeText should localize raw strings", () => {
+    setLocalizationDictionary(greekDictionary)
     expect(localizeText("Hello")).toBe("Γεια σας")
+})
+
+test("localizeText should return the input if no localization is found", () => {
+    setLocalizationDictionary(greekDictionary)
+    expect(localizeText("Not found")).toBe("Not found")
+})
+
+test("localizeText should handle edge cases gracefully", () => {
+    setLocalizationDictionary(greekDictionary)
+    expect(localizeText("")).toBe("")
+    expect(localizeText(null)).toBe(null)
+})
+
+test("l10n tag should handle raw strings", () => {
+    setLocalizationDictionary(greekDictionary)
+    expect(l10n`Hello`).toBe("Γεια σας")
+})
+
+test("l10n tag should handle interpolated strings", () => {
+    setLocalizationDictionary(greekDictionary)
+    const count = 7
+    expect(l10n`${count} notifications are available`).toBe("7 ειδοποιήσεις είναι διαθέσιμες")
+    expect(l10n`I can change order ${count}`).toBe(`${count} μπορώ να αλλάξω σειρά`)
+
+    const boxes = 3
+    const balls = 2
+    expect(l10n`I have ${boxes} boxes and ${balls} balls`).toBe(`Έχω ${boxes} κουτιά και ${balls} μπάλλες`)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-export let l10nDictionary: { [key: string]: string } = {}
+export interface Dictionary {
+    [key: string]: string
+}
+
+let l10nDictionary: Dictionary = {}
+
+export const setLocalizationDictionary = (dictionary: Dictionary) => {
+    l10nDictionary = dictionary
+}
 
 /** localizeText translates the given text using the currentDictionary. */
 export const localizeText = (text: string): string => {
     return l10nDictionary[text] || text
 }
 
-/** localize is the 'tag' function that translates a tagged template literal. */
-export const localize = (strings: string[], ...values: any[]): string => {
+/** localize is used by the l10n 'tag' function. */
+const localize = (strings: readonly string[], ...values: any[]): string => {
     let input: string
 
     if (strings.length == 1) {
@@ -19,11 +27,16 @@ export const localize = (strings: string[], ...values: any[]): string => {
             input += `{${i - 1}}${strings[i++]}`
         }
 
-        const template = localizeText(input)
+        const localizedTemplate = localizeText(input)
 
         return values.reduce(
             (result, value, i) => result.replace(`{${i}}`, value),
-            template,
+            localizedTemplate,
         )
     }
+}
+
+/** l10n is the 'tag' function that translates a tagged template literal. */
+export const l10n = (strings: TemplateStringsArray, ...values: any[]): string => {
+    return localize(strings.raw, ...values)
 }
