f759c94652ad5f891570339103af71ae86fca63e
George Moschovitis <gmosx@reizu.com>
2019-12-12 12:40:20 +0200

    format

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,52 +1,52 @@
 import { lc, localizeText, setLocalizationDictionary } from "./index";
 
 const greekDictionary = {
-    "Hello": "Γεια σας",
+    Hello: "Γεια σας",
     "{0} notifications are available": "{0} ειδοποιήσεις είναι διαθέσιμες",
     "I can change order {0}": "{0} μπορώ να αλλάξω σειρά",
     "I have {0} boxes and {1} balls": "Έχω {0} κουτιά και {1} μπάλλες",
-}
+};
 
 const germanDictionary = {
-    "Hello": "Hallo",
-}
+    Hello: "Hallo",
+};
 
 test("setLocalizationDictionary should change the localization language", () => {
-    setLocalizationDictionary(greekDictionary)
-    expect(localizeText("Hello")).toBe("Γεια σας")
+    setLocalizationDictionary(greekDictionary);
+    expect(localizeText("Hello")).toBe("Γεια σας");
 
-    setLocalizationDictionary(germanDictionary)
-    expect(localizeText("Hello")).toBe("Hallo")
-})
+    setLocalizationDictionary(germanDictionary);
+    expect(localizeText("Hello")).toBe("Hallo");
+});
 
 test("localizeText should localize raw strings", () => {
-    setLocalizationDictionary(greekDictionary)
-    expect(localizeText("Hello")).toBe("Γεια σας")
-})
+    setLocalizationDictionary(greekDictionary);
+    expect(localizeText("Hello")).toBe("Γεια σας");
+});
 
 test("localizeText should return the input if no localization is found", () => {
-    setLocalizationDictionary(greekDictionary)
-    expect(localizeText("Not found")).toBe("Not found")
-})
+    setLocalizationDictionary(greekDictionary);
+    expect(localizeText("Not found")).toBe("Not found");
+});
 
 test("localizeText should handle edge cases gracefully", () => {
-    setLocalizationDictionary(greekDictionary)
-    expect(localizeText("")).toBe("")
-    expect(localizeText(null)).toBe(null)
-})
+    setLocalizationDictionary(greekDictionary);
+    expect(localizeText("")).toBe("");
+    expect(localizeText(null)).toBe(null);
+});
 
 test("lc tag should handle raw strings", () => {
-    setLocalizationDictionary(greekDictionary)
-    expect(lc`Hello`).toBe("Γεια σας")
-})
+    setLocalizationDictionary(greekDictionary);
+    expect(lc`Hello`).toBe("Γεια σας");
+});
 
 test("lc tag should handle interpolated strings", () => {
-    setLocalizationDictionary(greekDictionary)
-    const count = 7
-    expect(lc`${count} notifications are available`).toBe("7 ειδοποιήσεις είναι διαθέσιμες")
-    expect(lc`I can change order ${count}`).toBe(`${count} μπορώ να αλλάξω σειρά`)
+    setLocalizationDictionary(greekDictionary);
+    const count = 7;
+    expect(lc`${count} notifications are available`).toBe("7 ειδοποιήσεις είναι διαθέσιμες");
+    expect(lc`I can change order ${count}`).toBe(`${count} μπορώ να αλλάξω σειρά`);
 
-    const boxes = 3
-    const balls = 2
-    expect(lc`I have ${boxes} boxes and ${balls} balls`).toBe(`Έχω ${boxes} κουτιά και ${balls} μπάλλες`)
-})
+    const boxes = 3;
+    const balls = 2;
+    expect(lc`I have ${boxes} boxes and ${balls} balls`).toBe(`Έχω ${boxes} κουτιά και ${balls} μπάλλες`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,39 @@
 interface Dictionary {
-    [key: string]: string
+    [key: string]: string;
 }
 
-let localizationDictionary: Dictionary = {}
+let localizationDictionary: Dictionary = {};
 
 /** setLocalizationDictionary sets the dictionary used for localization. */
 export const setLocalizationDictionary = (dictionary: Dictionary) => {
-    localizationDictionary = dictionary
-}
+    localizationDictionary = dictionary;
+};
 
 /** localizeText translates the given text using the currentDictionary. */
 export const localizeText = (text: string): string => {
-    return localizationDictionary[text] || text
-}
+    return localizationDictionary[text] || text;
+};
 
 /** localize is used by the lc 'tag' function. */
 const localize = (strings: readonly string[], ...values: any[]): string => {
-    let input: string
+    let input: string;
 
     if (strings.length == 1) {
-        return localizeText(strings[0])
+        return localizeText(strings[0]);
     }
 
-    input = strings[0]
+    input = strings[0];
 
     for (let i = 1; i < strings.length; i++) {
-        input += `{${i - 1}}${strings[i]}`
+        input += `{${i - 1}}${strings[i]}`;
     }
 
-    const localizedTemplate = localizeText(input)
+    const localizedTemplate = localizeText(input);
 
-    return values.reduce(
-        (result, value, i) => result.replace(`{${i}}`, value),
-        localizedTemplate,
-    )
-}
+    return values.reduce((result, value, i) => result.replace(`{${i}}`, value), localizedTemplate);
+};
 
 /** lc is the 'tag' function that localizes a tagged template literal. */
 export const lc = (strings: TemplateStringsArray, ...values: any[]): string => {
-    return localize(strings.raw, ...values)
-}
+    return localize(strings.raw, ...values);
+};
